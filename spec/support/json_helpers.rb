aaad30434fe7da1192036ca82e5b6b02eee2228b
Dev Raj <rehansingh.4522@gmail.com>
2020-03-02 15:12:53 +0530

    Forked github classroom

--- spec/support/json_helpers.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module JsonHelpers
-  def json
-    JSON.parse(response.body)
-  end
-end
--- spec/support/knapsack.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "knapsack"
-
-# CUSTOM_CONFIG_GOES_HERE
-
-Knapsack::Adapters::RSpecAdapter.bind
